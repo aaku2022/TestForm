b5ddc0d58548f4f4b1bc946e803b720699d315ea
Sumit Bhirud <sumitbhirud@Sumits-MacBook-Pro.local>
2022-07-13 21:14:09 +0200

    key value pair

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
-import { Component } from '@angular/core';
-import { __values } from 'tslib';
+import {Component} from '@angular/core';
+import {__values} from 'tslib';
 import UsersJson from './users.json';
-  
+
 interface Report {
-    ReportName: String;
-    Accountingperiod: String;
-    Category: {
-      Row: String;
-      Section: String;
-      Dashboard: String;
-      Report: String;
-    }
-    SectionIdentifier: String;
-    UserID: String;
-    Timestamp: String;
-    Comment: String;
+  ReportName: String;
+  Accountingperiod: String;
+  Category: {
+    Row: String;
+    Section: String;
+    Dashboard: String;
+    Report: String;
+  }
+  SectionIdentifier: String;
+  UserID: String;
+  Timestamp: String;
+  Comment: String;
 }
 
 interface Obj {
@@ -29,20 +29,25 @@ interface Obj {
 })
 export class AppComponent {
   title = 'FormTest1';
-  Users: Report[] = UsersJson;
+  Users = UsersJson;
   Keys = [""];
   Values = [""];
-  constructor(){
-    console.log(this.Users);
-    for (let key of this.Users) {
-      console.log("value?"+key);
-      for(var i in key){
-           this.Keys.push(i);
-           console.log('key: ' +  i + 'value: ' + key);
-      }
-     }
-    this.Keys.shift();
-    console.log(this.Keys);
+
+  constructor() {
+    // console.log(this.Users);
+    // for (let key of this.Users) {
+    //   console.log("value?" + key);
+    //   for (var i in key) {
+    //     this.Keys.push(i);
+    //     console.log('key: ' + i + 'value: ' + key);
+    //   }
+    // }
+    // this.Keys.shift();
+    // console.log(this.Keys);
+  }
+
+  ngOnInit() {
+
   }
 
 }
